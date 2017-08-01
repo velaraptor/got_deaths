c802691161a54e2919aa4dcb998d108f5fde3c38
Chris Vela <christophvel@gmail.com>
2017-08-01 16:22:09 -0500

    add sqlite db

--- shiny/server.R
+++ shiny/server.R
@@ -5,7 +5,7 @@ ep2=read.csv("got_predictions_EP2_fixed.csv")
 
 
 not_dead_predictions=read.csv("got_predictions_EP3_fixed.csv")
-
+##▼▲
 ge=merge(not_dead_predictions,ep2[,c(1,3)],by="name")
 ge$dead_rate_change=ge$dead_prob.x-ge$dead_prob.y
 
@@ -19,7 +19,7 @@ not_dead_predictions$highprob=as.numeric(not_dead_predictions$highprob)
 not_dead_predictions=merge(not_dead_predictions,ge[,c(1,9)],by='name')
 not_dead_predictions=not_dead_predictions[,c(1:5,8,6,7)]
 server = function(input, output) {
-  ndp=datatable(not_dead_predictions,options = list(order=list(1,"desc"),columnDefs = list(list(className = 'dt-center', targets = 1),list(targets = 7, visible = FALSE)),
+  ndp=datatable(not_dead_predictions,options = list(order=list(1,"desc"),columnDefs = list(list(className = 'dt-center', targets = c(1,5)),list(targets = 7, visible = FALSE)),
                                                     pageLength = 20, autoWidth = TRUE), filter = 'top',class = 'hover',
                 escape = FALSE,rownames=F,colnames = c('Name', 'Probability of Death', 'Current Location', 'Main House Interaction', 'House Allegiance',"Change from Previous EP","Character Image","High Prob")) %>% formatPercentage('dead_prob', 2) %>% formatPercentage('dead_rate_change', 2) %>% formatStyle(
                   "highprob",
